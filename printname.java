350bc1d0fb35517ca6bca302943beaad623c785d
jesamaesalado <95740917+lorens69@users.noreply.github.com>
2022-09-08 07:38:00 +0800

    Update printname.java

--- printname.java
+++ printname.java
@@ -2,5 +2,7 @@ public class FirstGroup {
     public static void main(String args[]) {
       System.out.println("Rob Wilson Caldosa");
 	System.out.println("Allan Divino");
+    System.out.println("John Laurence Castillo");
+
     }
 }
