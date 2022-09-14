74f855410471693df363dde8995bfb5b9b77e048
John Laurence Castillo <21103853@usc.edu.ph>
2022-09-14 08:26:02 +0800

    Added a main class Coffee

--- activities.java
+++ activities.java
@@ -1,8 +1,15 @@
 public class FirstGroup {
     public static void main(String args[]) {
-      System.out.println("Rob Wilson Caldosa");
+    System.out.println("Rob Wilson Caldosa");
 	System.out.println("Allan Divino");
     System.out.println("John Laurence Castillo");
 
+    }
+}
+
+class Coffees {
+    String benefit = "Good for your health.";
+    public void sound() {
+        System.outprintln("Slurp slurp";)
     }
 }
