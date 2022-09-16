a7b3f62c950cb2e0244052cf318d503f74827439
John Laurence Castillo <21103853@usc.edu.ph>
2022-09-16 12:57:20 +0800

    added Division function

--- activities.java
+++ activities.java
@@ -18,6 +18,17 @@ public class activities {
         return x - y;
       }
 
+      //DIVISION FUNCTION
+      static int divideMethodInt(int x, int y) {
+        return x / y;
+      }
+      
+      static double divideMethodDouble(double x, double y) {
+        return x / y;
+      }
+
+      
+
     public static void main(String args[]) {
         System.out.println("Rob Wilson Caldosa");
 	    System.out.println("Allan Divino");
