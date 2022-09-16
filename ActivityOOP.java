e2448a354fb9b9179e543fd98908a02957c6a353
John Laurence Castillo <21103853@usc.edu.ph>
2022-09-16 13:12:15 +0800

    added Activity 2 Part 2

---
+++ ActivityOOP.java
@@ -0,0 +1,27 @@
+public class ActivityOOP {
+  
+private String brandname;
+private int brandprice;
+
+public String Getter() {
+    return brandname;
+    return brandprice;
+}
+
+public void Setter (String brand, int price){
+    this.brandname = brand;
+    this.brandprice = price;
+}
+
+public void sound() {   
+        System.out.println("Slurp slurp");
+    }
+
+    public static void main(String args[]) {
+      
+      
+
+    }
+}
+
+
