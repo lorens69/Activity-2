65c023476da53ee2d703d629f2b13ca806a4922b
John Laurence Castillo <21103853@usc.edu.ph>
2022-09-16 12:51:22 +0800

    added Addition function

--- activities.java
+++ activities.java
@@ -1,5 +1,14 @@
 public class activities {
   
+    //ADDITION FUNCTION
+    static int plusMethodInt(int x, int y) {
+        return x + y;
+      }
+      
+      static double plusMethodDouble(double x, double y) {
+        return x + y;
+      }
+
     public static void main(String args[]) {
         System.out.println("Rob Wilson Caldosa");
 	    System.out.println("Allan Divino");
