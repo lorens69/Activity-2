c5d9596fe3df6fc5e1df6491d797010cea26320c
PNP-ACER07\Student.Admin <marygracevellina@gmail.com>
2022-09-16 14:48:48 +0800

    added objects in files

---
+++ Icecream.java
@@ -0,0 +1,34 @@
+class Icecream {
+    
+    private String icecreambrand;
+    int price = 120;
+    
+    public void feeling() {
+        System.out.println("Cold Delicious");
+    }
+    
+    
+    public String Getter() {
+        return icecreambrand;
+    }
+    
+    public void Setter (String Icecream){
+        this.icecreambrand = Icecream;
+    }
+    
+    }
+    
+    class Flavor extends Icecream{
+        
+        private final String taste = "this icecream is so yummy delecious";
+        
+        public static void main(String[] args) {
+        
+           System.out.println("Allan Divino" + ""); 
+           
+           Flavor f = new Flavor();
+           f.feeling();
+           f.Setter("Selecta");
+           System.out.println(f.Getter() + "," + f.taste + ", for the price of " + f.price + " pesos.");
+        }
+    } 
