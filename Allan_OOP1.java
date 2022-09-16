a6dc9c5fb98044624ee2c5a26716cdcad4a9b11b
PNP-ACER07\Student.Admin <marygracevellina@gmail.com>
2022-09-16 16:10:46 +0800

    Drystan Added

---
+++ Allan_OOP1.java
@@ -0,0 +1,25 @@
+public class ActivityOOP {
+  
+private String icecreambrand;
+private int icecreamprice;
+
+public String Getter() {
+    return isecreambrand;
+    return icecreamprice;
+}
+
+public void Setter (String brand, int price){
+    this.icecreambrand = brand;
+    this.icecreamprice = price;
+}
+
+public void feeling() {   
+        System.out.println("Cold Delicious");
+    }
+
+    public static void main(String args[]) {
+      
+      
+
+    }
+}
