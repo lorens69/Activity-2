9a52ea0713ae35ea90bdcea6479b5ee798fe8a1c
John Laurence Castillo <21103853@usc.edu.ph>
2022-09-16 14:10:27 +0800

    added objects

--- Coffee.java
+++ Coffee.java
@@ -1,27 +1,34 @@
-public class ActivityOOP {
-  
-private String brandname;
-private int brandprice;
-
-public String Getter() {
-    return brandname;
-    return brandprice;
-}
-
-public void Setter (String brand, int price){
-    this.brandname = brand;
-    this.brandprice = price;
-}
-
-public void sound() {   
+class Coffee {
+    
+    private String brandname;
+    int price = 12;
+    
+    public void sound() {
         System.out.println("Slurp slurp");
     }
-
-    public static void main(String args[]) {
-      
-      
-
+    
+    
+    public String Getter() {
+        return brandname;
     }
-}
-
-
+    
+    public void Setter (String CofeeBrand){
+        this.brandname = CofeeBrand;
+    }
+    
+    }
+    
+    class Bens extends Coffee{
+        
+        private final String benefit = "this coffee has a lot of benefits and shall make you happy";
+        
+        public static void main(String[] args) {
+        
+           System.out.println("John Laurence Castillo" + ""); 
+           
+           Bens mih = new Bens();
+           mih.sound();
+           mih.Setter("Kopiko");
+           System.out.println(mih.Getter() + "," + mih.benefit + ", and it only costs " + mih.price + " pesos.");
+        }
+    } 
